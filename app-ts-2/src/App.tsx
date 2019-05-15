04cd55e5ebfd722f8022e5abfdd8a6ded1006354
yagisuke <yagisuke.0209@gmail.com>
2019-05-15 21:13:23 +0900

    simple counter

--- app-ts-2/src/App.tsx
+++ app-ts-2/src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import { Store } from './Store'
 
+type ActionType = {
+  type: 'ADD' | 'SUB' | 'RES'
+}
+
 export default function App(): JSX.Element {
   const store = React.useContext(Store)
+
+  const reducer = (state: number, action: ActionType): number => {
+    switch(action.type) {
+      case 'ADD': return state +1
+      case 'SUB': return state -1
+      case 'RES': return 0
+    }
+  }
+  const [count, dispatch] = React.useReducer(reducer, 0)
 
   return (
     <>
       {console.log(store)}
       <h1>Rick and Morty</h1>
       <p>Pick your favourites episode!!</p>
+      <section>
+        <h1>Simple Counter</h1>
+        <div>{count}</div>
+        <button type="button" onClick={() => dispatch({ type: 'ADD' })}>+</button>
+        <button type="button" onClick={() => dispatch({ type: 'SUB' })}>-</button>
+        <button type="button" onClick={() => dispatch({ type: 'RES' })}>reset</button>
+      </section>
     </>
   )
 }
